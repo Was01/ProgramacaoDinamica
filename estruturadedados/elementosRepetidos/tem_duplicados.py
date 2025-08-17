8ceb71c7f83f61ca0c65f191a199d09dec475a5b
Was01 <80862278+Was01@users.noreply.github.com>
2025-08-17 18:13:38 -0300

    Add files via upload

---
+++ estruturadedados/elementosRepetidos/tem_duplicados.py
@@ -0,0 +1,12 @@
+def tem_duplicados(lista):
+    for i in range(len(lista)-1):
+        for j in range(i+1,len(lista)):
+            if (lista[i]==lista[j]):
+                return True
+    return False
+
+
+
+lista='Uva'
+
+print(tem_duplicados(lista))
