db9e1dc7fafaf9d48651d3df9817c54a4ec7a257
Was01 <80862278+Was01@users.noreply.github.com>
2026-01-06 13:46:52 +0000

    Add files via upload

--- analisedealgoritmos/inverte_lista/inverte_lista.py
+++ analisedealgoritmos/inverte_lista/inverte_lista.py
@@ -1,12 +1,28 @@
 def inverte_lista(lista):
     tamanho=len(lista)
     limite=tamanho//2
-    for i in range (limite):
-        lista[i],lista[tamanho-1-i]=lista[tamanho-1-i],lista[i]
+    for i in range(limite):
+        aux=lista[i]
+        lista[i]=lista[tamanho-1-i]
+        lista[tamanho-1-i]=aux
     return lista
-       
-       
- # Teste
- 
-minha_lista=[10,20,30,40,50]
-print(inverte_lista(minha_lista))
+
+
+
+def inverte_lista2(lista):
+    nova_lista=[]
+    tamanho=len(lista)
+    for i in range(tamanho):
+        nova_lista.append(lista[tamanho-1-i])
+    return nova_lista
+
+lista=[1,2,3,4,5]
+
+lista2=inverte_lista(lista)
+
+
+print(lista2)
+print(inverte_lista(lista2))
+
+
+
