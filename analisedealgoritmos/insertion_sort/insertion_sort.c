668aa9b43e8a4ea16f808ded512e7ad0e83cf384
Was01 <80862278+Was01@users.noreply.github.com>
2026-01-08 21:01:56 +0000

    Add files via upload

---
+++ analisedealgoritmos/insertion_sort/insertion_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void selection_sort(int A[], int n)
+{
+    int min_index, i, j, aux;
+    for (j = 0; j < n - 1; j++)
+    {
+        min_index = j;
+        for (i = j + 1; i < n; i++)
+        {
+            if (A[i] < A[min_index])
+            {
+                min_index = i;
+            }
+        }
+        if (A[j] > A[min_index])
+        {
+            aux = A[min_index];
+            A[min_index] = A[j];
+            A[j] = aux;
+        }
+    }
+}
+
+
+int main(){
+    int  vet[5]={7,5,1,8,3};
+    selection_sort(vet,5);
+    for(int i=0;i<5;i++){
+        printf("%d ",vet[i]);
+    }
+    return 0;
+}
