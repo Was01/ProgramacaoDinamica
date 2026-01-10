5741b0e4f9f12850ab0b97ce439a142fc00ae2a0
Was01 <80862278+Was01@users.noreply.github.com>
2026-01-10 10:08:03 -0300

    Create bubble_sort.c

---
+++ analisedealgoritmos/bubble_sort/bubble_sort.c
@@ -0,0 +1,12 @@
+void bubble_sort(int A[],int n){
+    int i,j,aux;
+    for(j=0;j<n-1;j++){
+        for(i=0;i<n-1-j;i++){
+            if(A[i]>A[i+1]){
+                aux=A[i];
+                A[i]=A[i+1];
+                A[i+1]=aux;
+            }
+        }
+    }
+}
